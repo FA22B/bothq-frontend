1643f27291d0f25ed2735c13140a8e2d695f5749
janm13 <jan.m13@schule.bremen.de>
2024-04-10 13:00:32 +0200

    Plugin-Showcase now uses variable dummy data

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {BHQPlugin} from "../../bhqplugin";
 
 @Component({
   selector: 'app-card',
@@ -8,6 +9,5 @@ import {Component, Input} from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-  @Input() isNew: boolean = false
-
+  @Input() bhqplugin!: BHQPlugin;
 }
--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {CardComponent} from "../card/card.component";
+import {BHQPlugin} from "../../bhqplugin";
 
 @Component({
   selector: 'app-carousel',
@@ -11,5 +12,11 @@ import {CardComponent} from "../card/card.component";
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent {
-
+  pluginList = [new BHQPlugin('Chuck Norris Jokes', 'Have the Bot tell a random Chuck Norris Joke from: https://api.chucknorris.io', true),
+    new BHQPlugin('Auto Role', 'Automatically assigns roles to users based on their activity.', false),
+    new BHQPlugin('Auto Voice', 'Automatically creates voice channels on demand and deletes them again, when that demand fades.', false),
+    new BHQPlugin('Auto Mod', 'Automatically moderates your server.', true),
+    new BHQPlugin('Auto Welcome', 'Automatically welcomes new members.', false),
+    new BHQPlugin('Auto Leave', 'Automatically says goodbye to members who leave.', true),
+    new BHQPlugin('Auto Nickname', 'Automatically changes nicknames.', false),]
 }
